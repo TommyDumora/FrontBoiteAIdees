1c5d26e33ffe85c292dd4ac0509efa3e499cb91b
Tommy Dumora <tommydumora@gmail.com>
2023-11-08 00:45:00 +0100

    récupération des idées

---
+++ src/services/apiService.js
@@ -0,0 +1,16 @@
+// import axios from 'axios'
+
+// const api = axios.create({
+//   baseURL: 'https://localhost:7031/api/'
+// })
+
+// export default {
+//   async getIdeas() {
+//     try {
+//       const response = await api.get('Ideas')
+//       return response.data
+//     } catch (error) {
+//       throw new Error('Impossible de récupérer les idées.')
+//     }
+//   }
+// }
--- src/views/ListIdeas.vue
+++ src/views/ListIdeas.vue
@@ -1,13 +1,51 @@
 <style scoped></style>
 
 <template>
-  <Layout> </Layout>
+  <Layout>
+    <main class="container">
+      <section class="list-idees">
+        <h1>Liste des idées</h1>
+
+        <ul class="contain-idee"></ul>
+      </section>
+    </main>
+  </Layout>
 </template>
 
 <script>
 import Layout from '../components/Layout.vue'
+// import apiService from '../services/apiService'
+import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      ideas: []
+      // loading: false
+    }
+  },
+  methods: {
+    async getAllIdeas() {
+      try {
+        const response = await axios.get('https://localhost:7031/api/Ideas')
+        this.ideas = response.data
+        console.log(this.ideas)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  },
+  async mounted() {
+    this.getAllIdeas()
+    // this.loading = true
+    // try {
+    //   this.ideas = await apiService.getIdeas()
+    // } catch (error) {
+    //   console.error(error)
+    // } finally {
+    //   this.loading = false
+    // }
+  },
   components: {
     Layout
   }
